606a3d7b3664eba5f663b90d2273f95ddf799131
OnesimusX <aubenseitergamez@gmail.com>
2026-03-19 09:19:23 -0500

    [EDIT] Refactored code in Player and Enemy

--- Source/OneTen/Public/Enemy/Enemy.h
+++ Source/OneTen/Public/Enemy/Enemy.h
@@ -22,37 +22,18 @@ class ONETEN_API AEnemy : public ACharacter, public IHitInterface
 	GENERATED_BODY()
 
 public:
-	
 	AEnemy();
 	
 	virtual void Tick(float DeltaTime) override;
+	
 	void CheckPatrolTarget();
 	void CheckCombatTarget();
+
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	virtual void GetHit_Implementation(const FVector& ImpactPoint) override;
-
 	void DirectionalHitReact(const FVector& ImpactPoint);
 
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser) override;
-
-
-protected:
-	
-	virtual void BeginPlay() override;
-
-	void Die();
-
-	bool InTargetRange(AActor* Target, double Radius);
-	void MoveToTarget(AActor* Target);
-	AActor* ChoosePatrolTarget();
-
-	UFUNCTION()
-	void PawnSeen(APawn* SeenPawn);
-
-	/**
-	* Play montage functions
-	*/
-	void PlayHitReactMontage(const FName& SectionName);
 
 private:
 
@@ -68,6 +49,25 @@ private:
 
 	UPROPERTY(VisibleAnywhere)
 	UPawnSensingComponent* PawnSensing;
+
+	/**
+    * Animation montages
+    */
+
+	UPROPERTY(EditDefaultsOnly, Category = Montages)
+	UAnimMontage* HitReactMontage;
+
+	UPROPERTY(EditAnywhere, Category = Sounds)
+	USoundBase* HitSound;
+
+	UPROPERTY(EditAnywhere, Category = VisualEffects)
+	UParticleSystem* HitParticles;
+
+	UPROPERTY(EditAnywhere, Category = Death)
+	TSubclassOf<ABreakableActor> BreakableSpawn;
+
+	UPROPERTY()
+	bool bHasDied = false;
 
 	/**
 	* Navigation
@@ -106,27 +106,22 @@ private:
 
 	EEnemyState EnemyState = EEnemyState::EES_Patrolling;
 
+protected:
+
+	virtual void BeginPlay() override;
+
+	void Die();
+
+	bool InTargetRange(AActor* Target, double Radius);
+	void MoveToTarget(AActor* Target);
+	AActor* ChoosePatrolTarget();
+
+	UFUNCTION()
+	void PawnSeen(APawn* SeenPawn);
 
 	/**
-	* Animation montages
+	* Play montage functions
 	*/
-	UPROPERTY(EditDefaultsOnly, Category = Montages)
-	UAnimMontage* HitReactMontage;
+	void PlayHitReactMontage(const FName& SectionName);
 
-	UPROPERTY(EditAnywhere, Category = Sounds)
-	USoundBase* HitSound;
-
-	UPROPERTY(EditAnywhere, Category = VisualEffects)
-	UParticleSystem* HitParticles;
-	
-	UPROPERTY(EditAnywhere, Category = Death)
-	TSubclassOf<ABreakableActor> BreakableSpawn;
-	
-	UPROPERTY()
-	bool bHasDied = false;
-
-
-
-
-	
 };
